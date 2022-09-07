fdb2596e0d14a598f04b1add8bdee223c1814d07
rodriguezl6355 <112957854+rodriguezl6355@users.noreply.github.com>
2022-09-07 09:31:23 -0500

    Update adding_numbers.py

--- adding_numbers.py
+++ adding_numbers.py
@@ -1,1 +1,10 @@
-
+def add(n1, n2):
+  if n1=="" or n2=="":
+   return"Invalid Operation"
+  if n1 is None or n2 is None:
+   return "Invalid Operation"
+  n1=int(n1)
+  n2=int(n2)
+  answer= n1 + n2
+  answer= str(answer)
+	return answer 
